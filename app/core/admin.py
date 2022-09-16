ed415b6920bc336d9eb89af7f3d41fbf1c264acd
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-09-16 22:46:19 +0200

    support modifying users admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -10,7 +11,22 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
-    list_display = ['email', 'name'] # type: ignore
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
