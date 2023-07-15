72225d89e0bbac9eec2bd84654f3439293b8cac8
mihaiandreineacsu <mihai@developerakademie.com>
2023-07-15 23:11:01 +0200

    subtask serializer id not required

--- app/subtask/serializers.py
+++ app/subtask/serializers.py
@@ -7,7 +7,8 @@ from core.models import Subtask
 
 class SubtaskSerializer(serializers.ModelSerializer):
     """Serializer for subtasks."""
+    id = serializers.IntegerField(required=False)
     class Meta:
         model = Subtask
         fields = ['id', 'title', 'done','created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
