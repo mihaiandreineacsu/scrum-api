993b4bb8008482f78d57cea50841b62a13213e72
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-01-08 21:33:32 +0100

    on update check for change list and change postition

--- app/task/views.py
+++ app/task/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Task
+from core.models import Task, List
 from task import serializers
 from django.db.models import Max, Min
 from django.db import IntegrityError
@@ -24,7 +24,7 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve tasks for authenticated user."""
-        queryset = self.queryset.filter(user=self.request.user)
+        queryset = self.queryset.filter(user=self.request.user).order_by("-position")
         # Get the query parameter
         list_is_null = self.request.query_params.get("list_is_null", None)
 
@@ -43,72 +43,48 @@ class TaskViewSet(viewsets.ModelViewSet):
         return self.serializer_class
 
     def create(self, request, **args):
-        list = request.data.get("list")
-        max_position = Task.objects.filter(list=list).aggregate(Max("position"))[
+        new_list = request.data.get("list")
+        max_position = Task.objects.filter(list=new_list).aggregate(Max("position"))[
             "position__max"
         ]
         request.data.update({"position": (max_position or 0) + 1})
         return super().create(request, *args)
 
     def update(self, request, *args, **kwargs):
-        # major difference, when we also wanna change the list
-        # we wanna update when different position as the instance has been posted
         task_instance = self.get_object()
-        new_position = request.data.get("position")
-        list = request.data.get("list", task_instance.list)
+        new_position = request.data.pop("position", None)
+        new_list = request.data.pop("list", task_instance.list)
 
-        print(
-            "Update Task {} at position {}, to position {} in list {}".format(
-                task_instance, task_instance.position, new_position, list
-            )
-        )
+        change_list = new_list != task_instance.list
+        change_position = new_position is not None and new_position != task_instance.position
 
-        # if list != task_instance.list:
-        #     self.swap_list(list, new_position, task_instance)
-        # elif new_position is not None:
-        #     self.swap_position(new_position, task_instance)
-
-        if new_position is not None or list != task_instance.list:
+        if change_list:
             try:
-                new_position = int(new_position)  # Ensure it's an integer
-                if task_instance.position != new_position:
-                    # Check if a task exists at the new position within the same list
-                    target_task = Task.objects.get(list=list, position=new_position)
-                    print(
-                        "Target task {} at position {} in list {}".format(
-                            target_task, target_task.position, target_task.list
-                        )
-                    )
-                    # Swap positions
-                    Task.swap_positions(task_instance, target_task)
-            except (ValueError, Task.DoesNotExist, IntegrityError):
-                # Handle invalid input or the case where no list exists with the new position
-                # You can return an appropriate response here if needed
-                return Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
+                max_position = Task.objects.filter(list=new_list).aggregate(Max("position"))["position__max"]
+                max_position = (max_position or 0) + 1
+                new_list_instance = List.objects.get(id=new_list)
+                Task.swap_parent(task_instance, new_list_instance, max_position)
+            except ValueError:
+                return Response("Invalid value type", status=status.HTTP_400_BAD_REQUEST)
+            except IntegrityError:
+                return Response("Request did not end successfully", status=status.HTTP_409_CONFLICT)
+            except List.DoesNotExist:
+                Task.swap_parent(task_instance, new_parent=None, new_position=max_position)
 
-            # if False and not list == task_instance.list:
-            #     target_task = Task.objects.get(list=list, position=task_instance.position)
-            #     print(
-            #         "Target task {} at position {} in list {}".format(
-            #             target_task, target_task.position, target_task.list
-            #         )
-            #     )
+        if change_position:
+            try:
+                new_position = int(new_position)
+                target_task = Task.objects.get(list=new_list, position=new_position)
+                Task.swap_positions(task_instance, target_task)
+            except ValueError as e:
+                return Response(f"Invalid value type: {e}", status=status.HTTP_400_BAD_REQUEST)
+            except IntegrityError as e:
+                return Response(f"Request did not end successfully: {e}", status=status.HTTP_409_CONFLICT)
+            except Task.DoesNotExist as e:
+                return Response(f"Bad Request: {e}", status=status.HTTP_404_NOT_FOUND)
 
-            # try:
-            #     return super().update(request, *args, **kwargs)
-            # except IntegrityError:
-            #     min_position = Task.objects.filter(list=list).aggregate(Min("position"))[
-            #         "position__min"
-            #     ]
-            #     return Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
         return super().update(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         """Create a new task."""
         task = serializer.save(user=self.request.user)
-
-    def swap_position(self, new_position, task_instance):
-        pass
-
-    def swap_list(self, new_list, new_position, task_instance):
-        pass
