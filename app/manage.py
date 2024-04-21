5fa08d7237d3e04fd93e543ce734f2b0dc45ccdd
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-04-21 11:26:26 +0200

    remove pydebug from manage.py

--- app/manage.py
+++ app/manage.py
@@ -9,15 +9,6 @@ def main():
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-    from django.conf import settings
-
-    if settings.DEBUG:
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            import debugpy
-            debugpy.listen(("0.0.0.0", 3000))
-            debugpy.wait_for_client()
-            print('Attached!')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
